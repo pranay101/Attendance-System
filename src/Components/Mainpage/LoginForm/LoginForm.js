ed2f0e0bec91009839f91db7f00944557fd3a732
pranay101 <pranayprajapati101@gmail.com>
2022-04-20 23:57:52 +0530

    Completed ui for desktop view

--- src/Components/Mainpage/LoginForm/LoginForm.js
+++ src/Components/Mainpage/LoginForm/LoginForm.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import classes from "./LoginForm.module.css"
+import React, { useState } from "react";
+import classes from "./LoginForm.module.css";
 const LoginForm = () => {
+  const [showLoginFormAdmin, setshowLoginFormAdmin] = useState(true);
   return (
     <div className={classes.LoginAdmin}>
-        <div className={classes.SelectLoginType}>
-            <p>Login As Admin</p>
-            <p>Login As Student</p>
-        </div>
-          <form action="" method="post">
-            <input type="email" name="" id="" />
-            <input type="password" name="" id="" />
-            <button type="submit">Login as Admin</button>
-          </form>
+      <div className={classes.SelectLoginType}>
+        <p onClick={() => setshowLoginFormAdmin(true)}>Login As Admin</p>
+        <p onClick={() => setshowLoginFormAdmin(false)}>Login As Student</p>
+      </div>
+      {
+      showLoginFormAdmin? (
+        <form action="" method="post">
+          <input type="email" name="" id="" />
+          <input type="password" name="" id="" />
+          <button type="submit">Login as Admin</button>
+        </form>
+      ) : 
+      (
+        <form action="" method="post">
+          <input type="email" name="" id="" />
+          <input type="password" name="" id="" />
+          <button type="submit">Login as Student</button>
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
