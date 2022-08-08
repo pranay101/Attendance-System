58db6e5e53d8971248293ad17387295b25b94cdc
SrishtiNimje <srishti.nimje12@gmail.com>
2022-08-08 18:04:22 +0530

    Router change

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RecoilRoot>
+  <React.StrictMode>
     <BrowserRouter>
-      <React.StrictMode>
+      <RecoilRoot>
         <App />
-      </React.StrictMode>
+      </RecoilRoot>
     </BrowserRouter>
-  </RecoilRoot>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
